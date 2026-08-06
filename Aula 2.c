280dfbddf7b55c505f1f84ed15eb3c58f514b0d0
hernandespsico-arch <hernandespsico@gmail.com>
2026-08-05 22:28:29 -0300

    Add files via upload

---
+++ Aula 2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	
+    int a,b,c;
+	float resultado; 
+	
+	a = 10;
+	b = 10;
+    c= 10;
+	
+    resultado = (a+b+c)/3;
+    
+    printf ("resultado %f", resultado);
+    
+	return 0;
+	
+}
