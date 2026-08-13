211262687be3e0d59451eab5193d862e81601452
hernandespsico-arch <hernandespsico@gmail.com>
2026-08-12 22:30:39 -0300

    Add files via upload

---
+++ Aula 4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+/*	int primeiro, segundo, aux;
+	
+	printf("Digite o primeiro Valor");
+	scanf("%d", &primeiro);
+		printf("Digite o segundo Valor");
+	scanf("%d", &segundo);
+	
+	aux = primeiro;
+	primeiro = segundo;
+	segundo = aux;
+	
+	printf("%d \n %d", primeiro, segundo); */
+	
+	//segundo exercício
+	
+	double Valor;
+	 
+	printf("Digite um Valor: ");
+	scanf("%lf", &Valor);
+	
+	printf("Notacao cientifica: %lf\n", Valor);
+	
+	//terceiro exercicio
+	
+	
+	return 0;
+}
